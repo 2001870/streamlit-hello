fd2d14c90427b6e5d78dce890d6bb2461251e321
2001870 <3183396080@qq.com>
2024-09-12 17:57:29 +0800

    Your commit message here

--- streanmlit.py
+++ streanmlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import time
-#st.write('# 陈雯静是大美女!2222')
+st.write('# 陈雯静是大美女!!!')
 
 #sex = st.text_input("sex","woman")
 
@@ -8,18 +8,18 @@ import time
 
 #st.write(f'input sex is {sex},age is {age}')
 
-def mock_login(uname,pwd):
-    time.sleep(1)
-    return uname=='JACK'and pwd =='123'
+#def mock_login(uname,pwd):
+    #time.sleep(1)
+    #return uname=='JACK'and pwd =='123'
 
 
-username=st.text_input('Username','JACK')
-password=st.text_input('Password','1234')
-if st.button('login'):
-    with st.spinner('loading..'):
-      login_result=mock_login(username,password)
-      text='登录成功' if login_result else'登录失败'
-      st.write(text)
+#username=st.text_input('Username','JACK')
+#password=st.text_input('Password','1234')
+#if st.button('login'):
+    #with st.spinner('loading..'):
+      #login_result=mock_login(username,password)
+      #text='登录成功' if login_result else'登录失败'
+      #st.write(text)
 
 
 
