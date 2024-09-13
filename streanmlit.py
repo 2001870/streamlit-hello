80834ad7e225f1558ee9b93eb0ec3e714a9660ca
2001870 <3183396080@qq.com>
2024-09-13 19:16:04 +0800

    init

--- streanmlit.py
+++ streanmlit.py
@@ -1,25 +1,3 @@
-#import streamlit as st
-#import time
-#st.write('# 陈雯静是大美女!!!')
-
-#sex = st.text_input("sex","woman")
-
-#age=st.number_input("Age",1,10,6)
-
-#st.write(f'input sex is {sex},age is {age}')
-
-#def mock_login(uname,pwd):
-    #time.sleep(1)
-    #return uname=='JACK'and pwd =='123'
-
-
-#username=st.text_input('Username','JACK')
-#password=st.text_input('Password','1234')
-#if st.button('login'):
-    #with st.spinner('loading..'):
-      #login_result=mock_login(username,password)
-      #text='登录成功' if login_result else'登录失败'
-      #st.write(text)
 import streamlit as st
 from sklearn.linear_model import LinearRegression
 import numpy as np
